36803553e5a5b587c7db46e2755f255ee765017a
Totoautoandtech <tomheude8@gmail.com>
2026-06-06 19:16:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+# On ajoute le dossier parent au chemin de recherche de Python
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import asyncio
 import logging
 import os
